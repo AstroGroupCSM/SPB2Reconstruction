4c385221aee6a030d9ac57a4740e91eb5b83b606
George Filippatos <gfilippatos@mines.edu>
2020-04-09 10:24:42 -0600

    Fixes

--- SPB2showerRec/TriggerCSM.cc
+++ SPB2showerRec/TriggerCSM.cc
@@ -18,6 +18,10 @@ TriggerCSM::Init()
   m2->Init();
   VModule* m3= VModuleFactory::Create("TriggerSPB2cells3x3TG");
   m3->Init();
+  VModule* m4= VModuleFactory::Create("TriggerSPB2TG");
+  m4->Init();
+  VModule* m5= VModuleFactory::Create("TriggerTG");
+  m5->Init();
   return eSuccess;
 }
 
@@ -37,7 +41,15 @@ TriggerCSM::Run(evt::Event& event)
   VModule* m3= VModuleFactory::Create("TriggerSPB2cells3x3TG");
   m3->Run(event);
   int triggerState2=event.HasTrigger();
-  std::cout<<"TriggerSPB2CSM\t"<<triggerState0<<"\tTriggerSPB2CellsTG\t"<<triggerState1<<"\tTriggerSPB2cells3x3TG\t" <<triggerState2<<std::endl;
+  event.SetTriggerState(0);
+  VModule* m4= VModuleFactory::Create("TriggerSPB2TG");
+  m4->Run(event);
+  int triggerState3=event.HasTrigger();
+  event.SetTriggerState(0);
+  VModule* m5= VModuleFactory::Create("TriggerTG");
+  m5->Run(event);
+  int triggerState4=event.HasTrigger();
+  std::cout<<"TRIGGERS\tTriggerSPB2CSM\t"<<triggerState0<<"\tTriggerSPB2CellsTG\t"<<triggerState1<<"\tTriggerSPB2cells3x3TG\t" <<triggerState2<<"\tTriggerSPB2TG\t" <<triggerState3<<"\tTriggerTG\t" <<triggerState4<<std::endl;
   return eSuccess;
 }
 
@@ -47,9 +59,4 @@ TriggerCSM::Finish()
   return eSuccess;
 }
 
-}
-
-// For special applications.
-void JemEusoOfflineUser()
-{
 }
