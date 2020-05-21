2de9b287a8760c7b13b34ef546a555de59881f2f
George Filippatos <gfilippatos@mines.edu>
2020-05-21 11:17:58 -0600

    Python Output fun

--- PythonOutput/PythonOutput.py
+++ PythonOutput/PythonOutput.py
@@ -1,8 +1,8 @@
 import numpy as np
 import sys
 import os 
-w=sys.argv[-1]
-F=int(sys.argv[-2])
+eventID=sys.argv[-1]
+outputDir=sys.argv[-2]
 M=[np.loadtxt('example-'+str(i)+'.txt') for i in range(128)]
 os.system('rm example-*.txt')
-np.save("event-"+str(w)+".npy",M)
+np.save(outputDir+"/event-"+eventID+".npy",M)
