5256f4b6e2917cd122973c78365044ae0e1fc5ad
George <gfilippatos@mines.edu>
2020-06-10 23:46:42 +0000

    From desktop-laptop

--- SPB2showerInspector/run.sh
+++ SPB2showerInspector/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for ((i=0;i<200;i+=25)); do sed "s/XXXXXX/$i/g" bootstrap.xml > bootstrap$i.xml &&./JemEusoOffline -b bootstrap$i.xml 2>&1|grep TRIGGERS >$i.txt & done;
+for ((i=0;i<200;i+=25)); do sed "s/XXXXXX/$i/g" bootstrap.xml > bootstrap$i.xml &&./JemEusoOffline -b bootstrap$i.xml 2>&1|grep QWERTY >$i.txt & done;
