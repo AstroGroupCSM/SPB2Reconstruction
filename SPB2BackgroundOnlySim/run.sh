6f48cfd84e38543398821f7b28687825599f572b
George Filippatos <gfilippatos@mines.edu>
2020-06-22 15:08:06 -0600

    New BG test

--- SPB2BackgroundOnlySim/run.sh
+++ SPB2BackgroundOnlySim/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for ((i=0;i<8;i++)); do ./JemEusoOffline  2>&1 |grep TRIGGERS> $i.txt & done;
+for ((i=0;i<8;i+=1)); do sed "s/XXXXXX/$i/g" bootstrap.xml > bootstrap$i.xml &&./JemEusoOffline -b bootstrap$i.xml 2>&1|grep TRIGGERS >$i.txt & done;
