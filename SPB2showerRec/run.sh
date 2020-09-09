975078f6bde09640cdae1f38cb410992bf700c52
George Filippatos <gfilppatos@mines.edu>
2020-09-09 08:51:50 -0600

    Ready to test triggering

--- SPB2showerRec/run.sh
+++ SPB2showerRec/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for ((i=0;i<200;i+=25)); do sed "s/XXXXXX/$i/g" bootstrap.xml > bootstrap$i.xml &&./JemEusoOffline -b bootstrap$i.xml 2>&1|grep TRIGGERS >$i.txt & done;
+for j in $(seq 17.8 0.1 19.7); do sed "s/XXXXXX/$j/g" bootstrap.xml > bootstrap$j.xml &&./JemEusoOffline -b bootstrap$j.xml 2>&1|grep TRIGGERS >$j.txt & done;
