b6cc9164e07419adfc46879573be1468a0f2a940
george <gfil@apartment-desktop-20.04>
2020-10-05 13:52:27 -0600

    Python Output

--- FSim-Trigger/run.sh
+++ FSim-Trigger/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-for j in $(seq 17.8 0.1 19.7); do sed "s/XXXXXX/$j/g" bootstrap.xml > bootstrap$j.xml &&./JemEusoOffline -b bootstrap$j.xml 2>&1|grep TRIGGERS >$j.txt & done;
+dir=pwd
+for j in $(seq 17.8 0.1 19.7); do  sed "s/XXXXXX/$j/g" bootstrap.xml > bootstrap$j.xml && ./JemEusoOffline -b ../bootstrap$j.xml 2>&1|grep TRIGGERS >$j.txt ; done;
